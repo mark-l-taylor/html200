3be0ebe6fc96cba5d44ed5b2bd0f2000568cc41e
Mark L Taylor <taylormarkld@gmail.com>
2022-05-09 20:14:46 -0700

    Updating for proper JavaScript syntax, forgot to end lines with semicolon

--- lesson03/challenge1.js
+++ lesson03/challenge1.js
@@ -1,13 +1,13 @@
-let fruit = ['apple', 'pear', 'strawberry', 'kiwi', 'banana']
-console.log(fruit)
+let fruit = ['apple', 'pear', 'strawberry', 'kiwi', 'banana'];
+console.log(fruit);
 
-console.log('\nAdd and element to the end of the array')
-fruit.push('raspberry')
-console.log(fruit)
+console.log('\nAdd and element to the end of the array');
+fruit.push('raspberry');
+console.log(fruit);
 
-console.log('\nRemove the third element...strawberry')
-fruit.splice(2, 1)
-console.log(fruit)
+console.log('\nRemove the third element...strawberry');
+fruit.splice(2, 1);
+console.log(fruit);
 
-console.log('\nCreate a string from the elements and comma seperate them')
-console.log(fruit.join(', '))
+console.log('\nCreate a string from the elements and comma seperate them');
+console.log(fruit.join(', '));
