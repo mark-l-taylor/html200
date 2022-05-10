939e07660559a4e0c9e9f9bce418d52c640ceea8
Mark L Taylor <taylormarkld@gmail.com>
2022-05-09 20:13:06 -0700

    Completed lesson3 challenge 2

---
+++ lesson03/challenge2.js
@@ -0,0 +1,9 @@
+let sentence = "It is a pleasure to meet you today.";
+console.log(sentence);
+
+console.log('\nTurn the sentence into an array');
+let arr = sentence.split(' ');
+console.log(arr);
+
+console.log('\nWhat is the third word');
+console.log(arr[2]);
